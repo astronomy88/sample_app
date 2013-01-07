a3db35184f5885c3c2c8f80f70c7bd58fe4fec87
Harold Nguyen <astronomy88@yahoo.com>
2013-01-07 13:48:48 -0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def show
   	#raise params.inspect
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -98,7 +98,19 @@ describe "AuthenticationPages" do
 					it { should have_selector('title', text: 'Sign in') }
 				end
 			end
+		end
 
+		describe "in the Microposts controller" do
+
+			describe "submitting to the create action" do
+				before { post microposts_path }
+				specify { response.should redirect_to(signin_path) }
+			end
+
+			describe "submitting to the destroy action" do
+				before { delete micropost_path(FactoryGirl.create(:micropost)) }
+				specify { response.should redirect_to(signin_path) }
+			end
 		end
 
 		describe "as wrong user" do
@@ -129,5 +141,7 @@ describe "AuthenticationPages" do
 				specify { response.should redirect_to(root_path) }
 			end
 		end
+
+
 	end
 end
